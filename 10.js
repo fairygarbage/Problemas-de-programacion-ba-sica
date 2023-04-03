4d3949d4bdce65cfa7bf915b491b948449173cd0
Jose Avila <joseriavila@gmail.com>
2023-04-03 12:35:35 -0600

    10.- Introducir un número por teclado y que nos diga si es par o impar.

---
+++ 10.js
@@ -0,0 +1,8 @@
+
+let numero = Number(prompt('Introduce un número: '));
+
+if (numero % 2 === 0) {
+    console.log(`El número ${numero} es par`);
+} else {
+    console.log(`El número ${numero} es impar`);
+}
